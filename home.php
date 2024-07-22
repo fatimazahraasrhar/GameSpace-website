37d9399978be299428ccf43e01e78f9dc11652b2
fatimazahraasrhar <fatimazahraasrhar@gmail.com>
2024-07-22 09:48:09 +0100

    https://github.com/fatimazahraasrhar/GameSpace-website.git

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <!-- swiper css link  -->
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <!-- font cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <!-- custom css file -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- header section starts  -->
+ <section class="header">
+ <a href="home.php" class="logo">Game</a>
+ <nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+</nav>
+ </section>
+<!-- header section ends  -->
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custon js file -->
+ <script src="js/script.js"></script>
+</body>
+</html>
