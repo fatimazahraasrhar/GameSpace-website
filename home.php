2971211aada373ad2274cbcdc6bcbd26d0536644
fatimazahraasrhar <fatimazahraasrhar@gmail.com>
2024-07-22 15:58:28 +0100

    add footer

--- home.php
+++ home.php
@@ -15,13 +15,14 @@
 <body>
 <!-- header section starts  -->
  <section class="header">
- <a href="home.php" class="logo">Game</a>
+ <a href="home.php" class="logo">GameSpace</a>
  <nav class="navbar">
     <a href="home.php">home</a>
     <a href="about.php">about</a>
     <a href="package.php">package</a>
     <a href="book.php">book</a>
 </nav>
+<div id="menu-btn" class="fas fa-bars"> </div>
  </section>
 <!-- header section ends  -->
 
@@ -30,10 +31,49 @@
 
 
 
+<!-- footer section starts -->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+        <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+        <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+        <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i>ask questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+        <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+        </div>
+
+        <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> 0668950581</a>
+        <a href="#"> <i class="fas fa-envelope"></i> Gamespace@gmail.com</a>
+        <a href="#"> <i class="fas fa-map"></i> Casablanca, morocco</a>
+        </div>
+
+        <div class="box">
+        <h3>follow us</h3>
+        <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+        <a href="#"> <i class="fab fa-x-twitter"></i> X</a>
+        <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+        <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+        </div>
+    </div>
+    <!-- <div class="credit"> created by miss fatima zahra asrhar </div> -->
+</section>
+
+
+
 <!-- swiper js link  -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
 <!-- custon js file -->
- <script src="js/script.js"></script>
+ <script src="script.js"></script>
 </body>
 </html>
